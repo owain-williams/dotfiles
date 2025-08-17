919308a40b9442440c6d131d49cbd8369aa09c3a
Owain Williams <owain@andersdigital.com>
2025-08-17 16:19:46 +0100

    add poimandres theme to lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,9 +2,42 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local poimandres_theme = {
+      normal = {
+        a = { fg = '#1b1e28', bg = '#ADD7FF', gui = 'bold' },
+        b = { fg = '#a6accd', bg = '#303340' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      },
+      insert = {
+        a = { fg = '#1b1e28', bg = '#5DE4c7', gui = 'bold' },
+        b = { fg = '#a6accd', bg = '#303340' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      },
+      visual = {
+        a = { fg = '#1b1e28', bg = '#d0679d', gui = 'bold' },
+        b = { fg = '#a6accd', bg = '#303340' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      },
+      replace = {
+        a = { fg = '#1b1e28', bg = '#fffac2', gui = 'bold' },
+        b = { fg = '#a6accd', bg = '#303340' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      },
+      command = {
+        a = { fg = '#1b1e28', bg = '#89ddff', gui = 'bold' },
+        b = { fg = '#a6accd', bg = '#303340' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      },
+      inactive = {
+        a = { fg = '#767c9d', bg = '#1b1e28' },
+        b = { fg = '#767c9d', bg = '#1b1e28' },
+        c = { fg = '#767c9d', bg = '#1b1e28' }
+      }
+    }
+
     require('lualine').setup({
       options = {
-        theme = 'gruvbox'
+        theme = poimandres_theme
       },
       tabline = {
         lualine_a = {'buffers'},
