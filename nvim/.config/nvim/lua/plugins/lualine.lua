adb369516eb5d084f5f3ee1ddda4d3f5fac6d0a8
Owain Williams <owain@andersdigital.com>
2025-08-17 15:28:56 +0100

    use lualine for buffers

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,14 @@ return {
     require('lualine').setup({
       options = {
         theme = 'gruvbox'
+      },
+      tabline = {
+        lualine_a = {'buffers'},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
       }
     })
   end
