cdb0fc5c030667056e50a4039e0d49441294cf83
Owain Williams <owain@andersdigital.com>
2025-08-17 13:40:59 +0100

    expand treesitter config to include more languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
   build = ":TSUpdate",
-  auto_install = true
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "graphql",
+        "html",
+        "css",
+        "json",
+        "markdown",
+        "yaml"
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true
+      }
+    })
+  end
 }
