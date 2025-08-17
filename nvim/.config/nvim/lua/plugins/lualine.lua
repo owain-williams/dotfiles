7b973d7481fc21672f5d8d2fc882bea34beb020e
Owain Williams <owain@andersdigital.com>
2025-08-17 17:32:58 +0100

    add branch and filename to top bar

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -41,8 +41,8 @@ return {
       },
       tabline = {
         lualine_a = {'buffers'},
-        lualine_b = {},
-        lualine_c = {},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {'tabs'}
